7797c44cfab7c18077118c39862a7c428a4c0464
Albin <albin7006@gmail.com>
2021-09-18 17:12:34 -0500

    create

---
+++ configurations/db-conf.js
@@ -0,0 +1,9 @@
+const host='LocalHost';
+const user= 'root';
+const database= 'escuela';
+//const password="ufk60pjd3xv5wxfm";
+
+exports.host=host;
+exports.user=user;
+//exports.password=password;
+exports.database=database;
